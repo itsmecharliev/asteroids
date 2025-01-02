7915c967029ae3937632d6e2768fd9054c3f3939
itsmecharliev <45504444+itsmecharliev@users.noreply.github.com>
2025-01-02 12:10:27 -0800

    Created base files

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pyagme.QUIT:
+                return
+        pygame.Surface.fill(screen, 0)
+        pygame.display.flip()
+
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
